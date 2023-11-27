7ae747551287999a332e7bcafbe85a5415b06023
brunolralves <brunolralves@yahoo.com>
2023-11-27 08:01:30 -0300

    Resolvidi problema da inserção da nota, entretanto há problema diferente com as tags, ainda investigando

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,34 +1,52 @@
 import AppError from '../utils/AppError.js';
 import knex from '../database/knex/index.js';
 import {randomUUID} from 'crypto';
+import { response } from 'express';
 
 
 class NotesController{
 
 	async create(req,res){	
 		const {title, description,tags,links}  = req.body;
-		const {user_id} = req.params;
+		const {user_id}	= req.params;
 
-		const UID_NOTE = randomUUID();
+		// const user_id = Number(userid);
+
+		// const UID_NOTE = randomUUID();
+		const id = randomUUID();
+
 		const [note_id] = await knex('notes').insert({
-			UID_NOTE,
+			id,
 			title,
 			description,
-			user_id		
+			user_id
 		});
 
+		console.log(note_id);
 
-		// const linksInsert = links.map(link =>{
-		// 	return{
-		// 		note_id,
-		// 		url: link
-		// 	};
-		// });
+		const linksInsert = links.map(link =>{
+			return{
+				note_id,
+				url: link
+			};
+		});
+		// console.log(linksInsert);
 
-		// await knex('links').insert(linksInsert);
+		await knex('links').insert(linksInsert);
 
-    
-		res.status(200);
+    // const id_tag
+		const tagsInsert = tags.map(name =>{
+			return{
+				note_id,
+				name,
+				user_id
+			};
+		});
+		
+		console.log(tagsInsert);
+		await knex('tags').insert('tagsInsert');
+
+		res.json();
 		
 	}
 
