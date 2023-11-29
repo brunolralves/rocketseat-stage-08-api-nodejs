f55246c9af6fd940672f62bbbf76519107ff4a2c
brunolralves <brunolralves@yahoo.com>
2023-11-29 07:56:55 -0300

    :sparkles: feat: Inclusas tags nas notas
    Incluso map para extrair as tags de uma nota e usuario e as exibir no endpoint show

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -112,8 +112,17 @@ class NotesController{
 				.whereLike('title',`%${title}%`)
 				.orderBy('note_id');	
 		}
+
+		const userTags = await knex('tags').where({user_id});
+		const notesWithTags = notes.map(note => {
+			const noteTags = userTags.filter(tag => tag.note_id === note.note_id);
+			return{
+				...note,
+				tags: noteTags
+			};
+		});
 			
-		res.json(notes);
+		res.json(notesWithTags);
 	}
 }
 
